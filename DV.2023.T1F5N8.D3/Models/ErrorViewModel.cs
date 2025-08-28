9204c0b099f5f818c0bfbc96f1ee23d2340dabd2
Enzo Rechner <enzo.rechner@gmail.com>
2025-08-28 21:44:54 +0200

    Initial commit

---
+++ DV.2023.T1F5N8.D3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DV._2023.T1F5N8.D3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
